98326221d0e29e88d0f2eb7726eaa76a0fe89174
prostov <rostov@newsystems.ru>
2021-11-10 16:54:37 +0300

    add func upload multiPart file to Naumen

--- Utils/CommonsUtils.cs
+++ Utils/CommonsUtils.cs
@@ -2,6 +2,11 @@ using System;
 using System.IO;
 using Microsoft.Extensions.Logging;
 using RestApiWithServiceWorker.Domain;
+using MimeTypes;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
 
 namespace Utils
 {
@@ -21,14 +26,35 @@ namespace Utils
             // System.Console.WriteLine(namelog + ": " + obj);
         }
 
+
         public byte[] getByteLocalFile(string pathTempDir)
         {
             byte[] buff = null;
-            FileStream fs = new FileStream(pathTempDir, FileMode.Open, FileAccess.Read);
-            BinaryReader br = new BinaryReader(fs);
-            long numBytes = new FileInfo(pathTempDir).Length;
-            buff = br.ReadBytes((int)numBytes);
-            return buff;
+            if (File.Exists(pathTempDir))
+            {
+                FileStream fs = new FileStream(pathTempDir, FileMode.Open, FileAccess.Read);
+                // FileStream fs = File.OpenRead(pathTempDir);
+                // fs.Name.get
+                // string v = Path.GetExtension(pathTempDir);
+                // Console.WriteLine(v);
+
+                // string contentType = MimeTypeMap.GetMimeType(Path.GetExtension(pathTempDir));
+                // Console.WriteLine(contentType);
+                // // File.Create();
+
+
+                BinaryReader br = new BinaryReader(fs);
+                long numBytes = new FileInfo(pathTempDir).Length;
+                buff = br.ReadBytes((int)numBytes);
+                fs.Close();
+                File.Delete(pathTempDir);
+                return buff;
+            }
+            else
+            {
+                printConsoleAndLogFile("File not exist to path", pathTempDir);
+                return new byte[0];
+            }
         }
 
         public string createUrl(MessageResponse messageResponse)
@@ -44,6 +70,28 @@ namespace Utils
 
             }
             return urlToRequestNaumen;
+        }
+
+        public async Task<string> Upload(byte[] data, string url, MessageResponse messageResponse, string path)
+        {
+            using (var client = new HttpClient())
+            {
+                using (var content = new MultipartFormDataContent())
+                {
+                    content.Headers.ContentType.MediaType = "multipart/form-data";
+                    StreamContent streamContent = new StreamContent(new MemoryStream(data));
+                    string contentType = MimeTypeMap.GetMimeType(Path.GetExtension(path));
+                    streamContent.Headers.ContentType = new MediaTypeHeaderValue(contentType);
+                    content.Add(streamContent, messageResponse.File, messageResponse.Fname);
+
+                    using (var message = await client.PostAsync(url, content))
+                    {
+                        var input = await message.Content.ReadAsStringAsync();
+                        // return response Naumen
+                        return input;
+                    }
+                }
+            }
         }
     }
 }
